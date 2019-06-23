aecd29eef7fb2ba6a0ab1526cf6e920978a4f812
yopaseopor <yopaseopor@gmail.com>
2019-06-23 03:13:02 +0200

    Update layerdef.js

--- map2/js_source/layerdef.js
+++ map2/js_source/layerdef.js
@@ -333,7 +333,7 @@ function layerdef(type){
 			),
 
 			make_layer(
-				QURL + "?data=(way[wheelchair=no][highway=footway](bbox);way[highway=steps](bbox););out+skel;",
+				QURL + "?data=(way['wheelchair'='no']['highway'='footway'](bbox);way['highway'='steps'](bbox););out+skel;",
 				name="#l#wheelchair=no<hr>",
 				defaultSolidLine("red"),
 				false,
